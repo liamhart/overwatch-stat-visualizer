90472c313a9b885ad96c3e401e1acc009b92499f
liamhart <liamh026@gmail.com>
2020-02-03 13:18:47 -0500

    Add files via upload

---
+++ config/express-session-options.js
@@ -0,0 +1,10 @@
+/**
+ * These properties are the options for express-session. For details, see:
+ * @link https://www.npmjs.com/package/express-session
+ * @type {{secret: string, resave: boolean, saveUnitialized: boolean}}
+ */
+module.exports={
+    secret: 'sufeafe9fje90j203fnwoenifwo',
+    resave: false,
+    saveUninitialized: false
+};
---
+++ config/mongodb-options.js
@@ -0,0 +1,8 @@
+/**
+ * How to connect to mongodb, URI may contain a password
+ * @type {{uri: string}}
+ */
+module.exports = {
+    uri: "mongodb://stat_trackerAdmin:cSVesIs049NM5snv@cluster0-shard-00-00-n5ktn.mongodb.net:27017,cluster0-shard-00-01-n5ktn.mongodb.net:27017,cluster0-shard-00-02-n5ktn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+    dbName: 'stat_tracker'
+};
